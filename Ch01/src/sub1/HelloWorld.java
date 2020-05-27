5254507ef8c88c680c6395bf7ddec7044c4f255b
kny2609 <kny2609@daum.net>
2020-05-27 11:11:21 +0900

    Ch01 등록

---
+++ Ch01/src/sub1/HelloWorld.java
@@ -0,0 +1,17 @@
+package sub1;
+/*
+ * 날짜 : 2020/04/29
+ * 이름 : 김나연
+ * 내용 : 자바 HelloWorld 출력하기
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		System.out.println("Hello Java!");
+		
+		float a = 3.123456789f;
+		System.out.println(a);
+	}
+
+}

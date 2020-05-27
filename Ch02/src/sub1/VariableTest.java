d3524bcd7714cc9f2a3802fdfc2688b76c1abace
kny2609 <kny2609@daum.net>
2020-05-27 11:17:46 +0900

    Ch02 등록

---
+++ Ch02/src/sub1/VariableTest.java
@@ -0,0 +1,23 @@
+package sub1;
+/*
+ * 날짜 : 2020/04/29
+ * 이름 : 김나연
+ * 내용 : 자바 변수 실습하기
+ */
+public class VariableTest {
+	public static void main(String[] args) {
+		
+		// 변수 : 데이터를 저장하는 메모리 공간
+		int num1 = 1;
+		int num2 = 2;
+		int a = 1;
+		int A = 1;
+		
+		int num3 = num1 + num2;
+		
+		System.out.println("num3 : "+num3);
+		System.out.println(a);
+		System.out.println(A);
+	}
+
+}

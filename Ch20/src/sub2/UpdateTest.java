0761d23e26abb6fb33896048c1bc8998b10be173
kny2609 <kny2609@daum.net>
2020-05-27 11:32:47 +0900

    Ch20 등록

---
+++ Ch20/src/sub2/UpdateTest.java
@@ -0,0 +1,44 @@
+package sub2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+/*
+ * 날짜 : 2020/05/26
+ * 이름 : 김나연
+ * 내용 : JDBC UPDATE 실습하기 
+ */
+public class UpdateTest {
+	public static void main(String[] args){
+		// DB정보
+		String host = "jdbc:mysql://192.168.44.7:3306/kny";
+		String user = "kny";
+		String pass = "1234";
+				
+		try {
+		// 1단계 - JDBC 드라이버 동적로드
+		Class.forName("com.mysql.jdbc.Driver");
+				
+		// 2단계 - 데이터베이스 접속
+		Connection conn = DriverManager.getConnection(host, user, pass);
+				
+		// 3단계 - SQL 실행객체 생성
+		Statement stmt = conn.createStatement();
+				
+		// 4단계 - SQL 실행
+		String sql = "UPDATE `USER1` SET `hp` = '010-1111-1111' ";
+			   sql += "WHERE `uid` = 'J101'";
+		stmt.executeUpdate(sql);
+				
+		// 5단계 - 결과셋(ResultSet) 처리(SELECT일 경우)
+				
+		// 6단계 - 데이터베이스 종료(해제)
+		conn.close();
+				
+	} catch (Exception e) {
+		System.out.println("쿼리문 실행에 문제가 발생했습니다.");
+	}
+	System.out.println("UPDATE 완료...");
+	}
+}

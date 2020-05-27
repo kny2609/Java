54d2e7ea6a9775c76d8fe0e7f7a300ba0f3cf795
kny2609 <kny2609@daum.net>
2020-05-27 12:20:39 +0900

    또 수정

--- Ch01/src/sub1/HelloWorld.java
+++ Ch01/src/sub1/HelloWorld.java
@@ -12,6 +12,11 @@ public class HelloWorld {
 		System.out.println("Hello Java");
 		System.out.println("Hello Java");
 		
+		System.out.println("Hello World~~");
+		System.out.println("Hello Java~~");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		
 	}
 
 }

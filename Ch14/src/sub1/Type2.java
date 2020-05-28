d9f5f6a493ea800dbaef47fceb6588258ced37f6
kny2609 <kny2609@daum.net>
2020-05-28 12:08:18 +0900

    Ch14 등록

---
+++ Ch14/src/sub1/Type2.java
@@ -0,0 +1,9 @@
+package sub1;
+
+@FunctionalInterface
+public interface Type2 {
+	
+	public void f2(int x);
+	
+	
+}
---
+++ Ch14/src/sub1/Type3.java
@@ -0,0 +1,7 @@
+package sub1;
+
+@FunctionalInterface
+public interface Type3 {
+	// abstract 생략가능
+	public abstract int f(int x, int y);
+}
